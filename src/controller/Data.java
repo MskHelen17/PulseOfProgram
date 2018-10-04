5755fa306a9df3bdadd31a0000848be896731beb
Mskhelen <Moskvitina_helen@mail.ru>
2018-10-04 17:12:46 +0300

    Example1

--- src/controller/Data.java
+++ src/controller/Data.java
@@ -1,6 +1,9 @@
 package controller;
 
 import java.util.ArrayList;
+import javax.xml.stream.XMLStreamReader;
+import java.nio.file.Paths;
+import java.nio.file.Files;
 import model.Button;
 import model.Task;
 import model.Test;
@@ -10,12 +13,16 @@ public class Data {
     private ArrayList<Button> buttons;
 
     public void readXmlData(){
-
+        try (StaxStreamProcessor processor = new StaxStreamProcessor(Files.newInputStream(Paths.get("../../../TestData/data.xml")))) {
+            XMLStreamReader reader = processor.getReader();
+            while (processor.startElement("button", "buttons")) {
+                System.out.println(processor.getAttribute("id") +":" + processor.getText());
+            }
+        }
     }
     public void generateTestData(){
 
     }
     public void writeXmlData(){
-
     }
 }
